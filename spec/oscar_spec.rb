b7dfd798fbc42d1555279d730a3673e44efb487e
Katie Hoffman <ktahoffman@gmail.com>
2015-01-23 18:55:58 -0500

    update spec errors

--- spec/oscar_spec.rb
+++ spec/oscar_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
-describe "" do
+describe "oscar list" do
 
   let(:oscars) { JSON.parse(File.read('spec/fixtures/oscars.json')) }
   let(:broken) { JSON.parse(File.read('spec/fixtures/broken.json')) }
   let(:animated_films) { ["Big Hero 6","Song of the Sea","The Boxtrolls","The Tale of the Princess Kaguya","How to Train Your Dragon 2"] }
 
+  # 1
   describe "#delete_teenage_mutant_ninja_turtles" do
     it "deletes the string Teenage Mutant Ninja Turtles from the array of best films" do
       result = delete_teenage_mutant_ninja_turtles(broken)
@@ -13,6 +14,7 @@ describe "" do
     end
   end
 
+  # 2
   describe "#add_birdman_to_first_index" do
     it "adds Birdman to the front of the best film array" do
       result = add_birdman_to_first_index(broken)
@@ -20,6 +22,7 @@ describe "" do
     end
   end
 
+  # 3
   describe "#replace_vampire_academy_with_into_the_woods" do
     it "replaces Vampire Academy with Into the Woods" do
       result = replace_vampire_academy_with_into_the_woods(broken)
@@ -28,21 +31,27 @@ describe "" do
     end
   end
 
+  # 4
   describe "#delete_best_movies_ever" do
-    it "deletes the key, value pair where the key is ':best_movies_ever'" do
+    it "deletes the key, value pair where the key is 'best_movies_ever'" do
       result = delete_best_movies_ever(broken)
-      expect(result["best_costumes"][2]).to eq("Into the Woods")
-      expect(result["best_costumes"]).to_not include("Vampire Academy")
+      expect(result.keys).to_not include("best_movies_ever")
+      expect(result.keys.length).to eq(2)
     end
   end
 
+  # 5
   describe "#add_best_animated_film" do
-    it "adds the key, value pair where the key is ':best_animated_films' 
-        and the value is the array of film titles" do
+    it "adds the key, value pair where the key is 'best_animated_films' 
+    and the value is the array of film titles" do
       result = add_best_animated_film(broken)
-      expect { result["add_best_animated_film"] }.to_not raise_error
-      expect(result.keys.length).to eq(3)
-      animated_films.each { |a| expect(result["add_best_animated_film"]).to include(a)}
+      expect { result["best_animated_films"] }.to_not raise_error
+      animated_films.each { |a| expect(result["best_animated_films"]).to include(a) }
+    end
+  end
+
+end
+ated_film"]).to include(a)}
     end
   end
 
